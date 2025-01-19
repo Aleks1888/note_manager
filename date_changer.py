d521931a97b2130a15a183eddba3e6f885cdda42
Aleks1888 <aleksandrsmirnov2855@gmail.com>
2025-01-19 14:30:52 +0300

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,13 @@
+username="имя пользователя"
+title="заголовок заметки"
+content="описание заметки"
+status="статус заметки"
+created_data='10-January-2025'
+issue_data='10-February-2025'
+
+print(username)
+print(title)
+print(content)
+print(status)
+print(created_data[0:10])
+print(issue_data[0:11])
