053d56c0ff5cfa6f86ddea9ab8f9970f7c948a5c
Александр <aleksandrsmirnov2855@gmail.com>
2025-01-19 16:06:10 +0300

    Add files via upload

---
+++ add_input.py
@@ -0,0 +1,13 @@
+username=input("имя пользователя")
+title=input("заголовок заметки")
+content=input("описание заметки")
+status=input("статус заметки")
+created_data='10-January-2025'
+issue_data='10-February-2025'
+
+print(username)
+print(title)
+print(content)
+print(status)
+print(f"начяло,{created_data[0:10]}")
+print(f"окончание,{issue_data[0:11]}")
