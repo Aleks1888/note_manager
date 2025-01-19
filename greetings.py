c259656aba8793634650168052c111dc40ec651e
Aleks1888 <aleksandrsmirnov2855@gmail.com>
2025-01-19 13:56:43 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,13 @@
+username="имя пользователя"
+title="заголовок заметки"
+content="описание заметки"
+status="статус заметки"
+created_data='10-01-2025'
+issue_data='10-02-2025'
+
+print(username)
+print(title)
+print(content)
+print(status)
+print(created_data)
+print(issue_data)
